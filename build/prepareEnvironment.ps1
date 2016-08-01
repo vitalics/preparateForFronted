ce43d8a1c614de93d4a8c87e7f2810da0a4ddd3b
Vitalicset <vitalicset@yandex.ru>
2016-08-01 15:24:23 -0700

    Switch code style to cmdlets

--- build/prepareEnvironment.ps1
+++ build/prepareEnvironment.ps1
@@ -1,10 +1,9 @@
 ﻿
-Set-ExecutionPolicy Unrestricted;
-
 [string] $scriptDir = Split-Path $script:MyInvocation.MyCommand.Path;
 Set-Location $scriptDir;
 
 [string]$programFiles;
+[string]$programFilesx86;
 if ([environment]::Is64BitOperatingSystem) {
     $programFilesx86 = "Program Files (x86)";
     $programFiles = "Program Files";
@@ -14,166 +13,181 @@ else
     $programFilesx86 ="Program Files";
     $programFiles = "Program Files";
 }
+[string] $VSInstalled = "C:\$programFilesx86\Microsoft Visual Studio 14.0\Common7\IDE\"
+[bool] $isVSInstalled = Test-Path $VSInstalled  -Filter .exe;
+if (-not $isVSInstalled) 
+{
+    $VSInstalled = "C:\$programFilesx86\Microsoft Visual Studio 15.0\Common7\IDE\"
+    $isVSInstalled = Test-Path $VSInstalled -Filter .exe;
+    Write-host "$isVSInstalled"
+}
 
-[bool] $isVSInstalled = Test-Path "C:\$programFilesx86\Microsoft Visual Studio 14.0\Common7\IDE\" -Filter .exe;
+[string] $NodeInstalled = "C:\$programFiles\nodejs\";
+[bool] $isNodeInstalled = Test-Path $NodeInstalled -Filter .exe;
 
-[bool] $isNodeInstalled = Test-Path "C:\$programFiles\nodejs\" -Filter .exe;
 
-[bool] $isRuby32Installed = Test-Path "C:\Ruby22-x64\";
 
-[bool] $isRuby64Installed = Test-Path "C:\Ruby22\";
+[string] $RubyInstalled = "C:\Ruby22\bin";
+[bool] $isRubyInstalled = Test-Path $RubyInstalled;
 
-[bool] $isPythonInstalled = Test-Path "C:\Python27";
+
+[string] $PythonInstalled = "C:\Python27";
+[bool] $isPythonInstalled = Test-Path $PythonInstalled;
 
 [string] $typescriptFolder = "C:\$programFilesx86\Microsoft SDKs\TypeScript\1.8\";
 
-$ENV:PATH | Select-String -SimpleMatch $typescriptFolder;
 
 [bool] $isTypescriptFolder = Test-Path $typescriptFolder;
 
 $arguments= ' /qn /l*v .\installers.txt';
 
-if(-not $isVSInstalled){
-    Write-Host "You don't have visual Studio 2015.Do you want install it (Y/N)";
-    $installCommand = Read-Host "Y/N";
-    if((($installCommand -eq "Y") -or ($installCommand -eq"y" ))){
-        try{
-            Start-Process -FilePath VS2015.1.exe|Resolve-Path -Relative -WindowStyle Hidden;
-        }
-        catch [System.InvalidOperationException]{
-            Write-Host "You canceled Visual Studio installer";
+
+
+function Check-Install-Programs
+{
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [Alias()]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [string]$PathToProgram,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true,
+                   Position=1)]
+        $pathToInstalledProgram,
+
+                [Parameter(Mandatory=$true,
+                   Position=2)]
+        [string]$NameOfProgram
+    )
+
+    Begin
+    {
+        if (-not((Test-Path $PathToProgram) -and (Test-Path "$scriptDir\$pathToInstalledProgram")))
+        {
+            return "error with path"  
         }
     }
-    else{
-        Write-Host "Check me later";
-        return;
+    Process
+    {
+        if(-not (test-path $PathToProgram)){
+            if ($PSCmdlet.ShouldContinue("You dont have $NameOfProgram, install it?",'Installer manager'))
+            {
+                try{
+                    Start-Process -FilePath $pathToInstalledProgram|Resolve-Path -Relative
+                    -arg $arguments
+                    -passthru | wait-process;
+                }
+                catch [System.InvalidOperationException]{
+                    Write-Host "You canceled installer";
+                } 
+            }
+            else{
+              return "You canceled installer program"  
+            }
+        }
+    }
+    End
+    {
+        Write-host "$NameOfProgram installed"
+        Write-host "-------------------------------------"
     }
 }
 
-Write-Host "OK VS";
+function Path-Checker
+{
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [Alias()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $PathToFolder,
 
-if(-not $isNodeInstalled){
-    Write-Host "You don't have Node.js.Do you want install it (Y/N)"
-    $installCommand = Read-Host "Y/N";
-    if((($installCommand -eq "Y") -or ($installCommand -eq"y" ))){
-        try{
-            Start-Process -FilePath node-v4.4.7-x86.msi|Resolve-Path -Relative
-            -arg $arguments
-            -passthru | wait-process;
-        }
-        catch [System.InvalidOperationException]{
-            Write-Host "You canceled node.js installer";
-        }
-    }
-    else{
-        Write-Host "Check me later";
-        return;
-    }
-}
-Write-Host "OK Node.js";
+        # Param2 help description
+        [string]
+        $NameOfProgram
+    )
 
+    Begin
+    {
+    $semicolon = ";";
+
+    $PathToFolder = $PathToFolder + $semicolon;
+
+    }
+    Process
+    {
     
-    if(-not $isPythonInstalled){
-        Write-Host "You don't have Python. do you want installed Python (Y/N)";
-        $installCommand = Read-Host "Y/N";
-        if(($installCommand -eq "Y") -or ($installCommand -eq"y" )){
-            try{
-                Start-Process python-2.7.11.msi|Resolve-Path -Relative
-                -arg $arguments
-                -passthru | wait-process;
-            }
-            catch [System.InvalidOperationException]{
-                Write-Host "You canceled python installer";
-            }
-    }
-    else{
-        Write-Host "Check me later";
-        return;
-    }
-}
-Write-Host "OK Python";
-
-
-if((-not $isRuby32Installed) -and (-not $isRuby64Installed)){
-    Write-Host "You don't have Ruby.Do you want install it (Y/N)?";
-    $installCommand = Read-Host "Y/N";
-    if(($installCommand -eq "Y") -or ($installCommand -eq"y" )){
-        try{
-            Start-Process -FilePath rubyinstaller-2.2.4.exe|Resolve-Path -Relative -WindowStyle Hidden -ArgumentList "\qn \s";
-        }
-        catch [System.InvalidOperationException]{
-            Write-Host "You canceled ruby installer";
-        }
-    }
-    else{
-        Write-Host "Check me later";
-        return;
-    }
-}
-
-Write-Host "OK Ruby";
-
-Write-Host "Do you want install gulp?(Y/N)";
-$installCommand = Read-Host "Y/N"
-if(($installCommand -eq "Y") -or ($installCommand -eq"y" )){
-    npm install --global gulp-cli
-}
-
-Write-Host "Do you want install sass?(Y/N)"
-$installCommand = Read-Host "Y/N"
-if(($installCommand -eq "Y") -or ($installCommand -eq"y" )){
-       gem install sass
-}
-
-Write-Host "Do you want install typescript?(Y/N)"
-$installCommand = Read-Host "Y/N"
-if(($installCommand -eq "Y") -or ($installCommand -eq"y" )){
-    npm install typescript -g
-}
-<# Adding typescript and ruby in to the path #>
-
-$semicolon = ";";
-$typescriptFolder = "C:\$programFiles\Microsoft SDKs\TypeScript\1.8";
-
-$ruby22Folder = 'C:\Ruby22\bin';
-$typescript = $semicolon + $typescriptFolder;
-$ruby22 = $ruby22Folder + $semicolon;
-
-IF (!(TEST-PATH $typescriptFolder))
-{ 
-    write-host ‘Folder Does not Exist, Cannot be added to PATH, do you want install it(Y/N)’
-    $installCommand = Read-Host "Y/N"
-    if(($installCommand -eq "Y") -or ($installCommand -eq"y" )){
-        try
+        if ($ENV:PATH | Select-String -SimpleMatch $PathToFolder)
         {
-            Start-Process -FilePath TypeScript_Dev14Full.exe|Resolve-Path -Relative
+            Return "Folder with $NameOfProgram already within PATH";
         }
-        catch [System.InvalidOperationException]
-        {
-            Write-Host "You canceled installer"
-        }   
+        else{
+            if ($PSCmdlet.ShouldContinue("install $NameOfProgram to PATH?","PATH Manager"))
+            {
+                [Environment]::SetEnvironmentVariable("Path",$env:Path + $PathToFolder);
+            }
+            else{
+                return "You canceled $NameOfProgram"
+            }
+        }
+    }
+    End
+    {
+        Write-host "-------------------------------------"
     }
 }
-elseif ($ENV:PATH | Select-String -SimpleMatch $typescript)
-{
-    Return ‘Folder with typescript already within PATH’ 
-}
-else{
-    Write-Host $env:Path;
-    [Environment]::SetEnvironmentVariable("Path",$env:Path + $typescript)
-    Write-host $env:Path;
-}
-#checking ruby
 
-if ($ENV:PATH | Select-String -SimpleMatch $ruby22)
+function Cli-installers
 {
-    Return ‘Folder with ruby already within PATH’ 
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [Alias()]
+    Param
+    (
+    )
+
+    Begin
+    {
+        
+    }
+    Process
+    {
+    if($PSCmdlet.ShouldContinue("Do you want install gulp?","NPM manager")){
+            npm install --global gulp-cli
+        }
+
+        if($PSCmdlet.ShouldContinue("Do you want install typescript?","NPM manager")){
+            npm install typescript -g
+        }
+
+
+        if($PSCmdlet.ShouldContinue("Do you want install sass?","Gem manager")){
+            gem install sass
+        }
+    }
+    End
+    {
+        Write-host "-------------------------------------"
+    }
 }
-else{
-    Write-Host $env:Path;
-    [Environment]::SetEnvironmentVariable("Path",$env:Path + $ruby22)
-    Write-host $env:Path;
-}
+
+Check-Install-Programs -PathToProgram $VSInstalled -pathToInstalledProgram 'VS2015.1.exe' -NameOfProgram "Visual Studio"
+
+Check-Install-Programs -PathToProgram $NodeInstalled -pathToInstalledProgram 'node-v4.4.7-x86.msi'-NameOfProgram "Node.js"
+
+Check-Install-Programs -PathToProgram $PythonInstalled -pathToInstalledProgram 'python-2.7.11.msi'-NameOfProgram "Python"
+
+Check-Install-Programs -PathToProgram $RubyInstalled -pathToInstalledProgram 'rubyinstaller-2.2.4.exe'-NameOfProgram "Ruby"
+
+Cli-installers
+
+Path-Checker -PathToFolder $RubyInstalled -NameOfProgram 'ruby'
+Path-Checker -PathToFolder $typescriptFolder -NameOfProgram 'typescript'
 
 Write-Host "Now start next script in your path with project";
-
+read-host;
